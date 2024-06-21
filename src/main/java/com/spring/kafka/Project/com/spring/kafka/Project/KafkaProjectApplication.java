366528b4a392d04c846154b00da6fd6a61d5b7e4
Sharanesh Hosamani <sharanesh.sonu1@gmail.com>
2024-06-21 16:52:09 +0530

    Kafka Project added

---
+++ src/main/java/com/spring/kafka/Project/com/spring/kafka/Project/KafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.Project.com.spring.kafka.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProjectApplication.class, args);
+	}
+
+}
